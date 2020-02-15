63c03c8ed23e375c714e6af77240be6a772dabd6
Abdul samad <studyonlinetech47@gmail.com>
2020-02-15 13:53:51 +0500

    autoLoad a class in php

---
+++ autoLoadClass/classes/Person.class.php
@@ -0,0 +1,11 @@
+<?php
+class Person{
+public $name = "Abdul samad";
+public $age = 21;
+
+public function display(){
+  echo $this->name . $this->age;
+}
+}
+
+ ?>
---
+++ autoLoadClass/index.php
@@ -0,0 +1,5 @@
+<?php
+include 'includes/autoLoad.inc.php';
+$per = new Person();
+$per->display();
+ ?>
